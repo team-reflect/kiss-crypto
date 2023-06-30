f52c66fc5d6b6226f61fd0c255badc7ca84c93a7
Alex MacCaw <alex@alexmaccaw.com>
2023-06-30 18:24:37 -0400

    add size limit

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  roots: ['<rootDir>/src'],
-  // transform: {
-  //   '^.+\\.ts?$': 'ts-jest',
-  // },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.ts?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-}
