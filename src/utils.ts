ab383fad6312aaea7e9aa094f0a1fe2e9820f535
Alex MacCaw <alex@alexmaccaw.com>
2023-07-01 22:00:54 -0400

    refactor

--- src/utils.ts
+++ src/utils.ts
@@ -17,17 +17,3 @@ export function generateRandomKey(length: number): string {
   const randomValues = randomBytes(length)
   return bytesToHex(randomValues)
 }
-
-export function concatUint8Arrays(...arrays: Uint8Array[]): Uint8Array {
-  const totalLength = arrays.reduce((acc, array) => acc + array.length, 0)
-  const result = new Uint8Array(totalLength)
-
-  let offset = 0
-
-  for (const array of arrays) {
-    result.set(array, offset)
-    offset += array.length
-  }
-
-  return result
-}
