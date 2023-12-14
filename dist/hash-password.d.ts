5f0e40aa10484395716a88d2575eb000f69cbd9c
ocavue <ocavue@gmail.com>
2023-12-14 23:17:44 +0800

    dist

---
+++ dist/hash-password.d.ts
@@ -0,0 +1,15 @@
+import type { HexString, Utf8String } from './types.js';
+/**
+ * Derives a key from a password and salt using
+ * argon2id (crypto_pwhash_ALG_DEFAULT).
+ * @param password - Plain text string
+ * @param salt - Hex salt string (use generateSalt())
+ * @returns Derived key in hex format
+ */
+export declare function hashPassword({ password, salt, iterations, bytes, length, }: {
+    password: Utf8String;
+    salt: HexString;
+    iterations?: number;
+    bytes?: number;
+    length?: number;
+}): Promise<HexString>;
